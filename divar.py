c6bcf4b69a281aacb02d8c6cdb048f4ca3b58d0d
sina majidi <sin.majidi@gmail.com>
2021-10-27 14:13:30 +0330

    i wrote a robot to take the apartments from 'divar.ir' in place of 'janat abad',janat abad is a region in tehran

--- divar.py
+++ divar.py
@@ -1,8 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
-page = requests.get("https://divar.ir/s/tehran/buy-apartment")
-soup = BeautifulSoup(page.content, 'html.parser')
-home_items = soup.find_all(class_="post-card-item kt-col-6 kt-col-xxl-4")
-for apartment in home_items:
-	if  'پونک' in apartment.get_text():
-		print(apartment.get_text())
+import time
+oldapartment=" "
+
+def scroll():
+	print("scroll")
+	return
+
+while True:
+	page = requests.get("https://divar.ir/s/tehran/buy-apartment")
+	soup = BeautifulSoup(page.content, 'html.parser')
+	home_items = soup.find_all(class_="post-card-item kt-col-6 kt-col-xxl-4")
+	for newapartment in home_items:
+		if  'جنت آباد' in newapartment.get_text():
+			if oldapartment!=newapartment.get_text():
+				print(newapartment.get_text())
+				oldapartment=newapartment.get_text()
+	# scroll()
+	time.sleep(20)
