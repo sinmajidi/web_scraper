803ef618ed291c8504ffe4ce54d71d94afb6af94
sina majidi <sin.majidi@gmail.com>
2021-10-27 20:35:19 +0330

    i want to take tellphone number of apartment seller using the link

--- divar.py
+++ divar.py
@@ -1,20 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
 import time
-oldapartment=" "
 
-def scroll():
-	print("scroll")
+oldapartment=" "
+base_url="https://divar.ir/s/tehran/buy-apartment"
+pre_href_url="https://divar.ir"
+
+def load():
+	page = requests.get(base_url)
+	soup = BeautifulSoup(page.content, 'html.parser')
+	return soup.find_all(class_="post-card-item kt-col-6 kt-col-xxl-4")
+
+def checklink(link):
+	print('href: ', link['href'])
+	request_href = requests.get(pre_href_url + link['href'])
+	soup = BeautifulSoup(request_href.content, 'html.parser')
+	tell = soup.find(class_="kt-button kt-button--primary post-actions__get-contact")
+	print(tell.prettify())
 	return
 
 while True:
-	page = requests.get("https://divar.ir/s/tehran/buy-apartment")
-	soup = BeautifulSoup(page.content, 'html.parser')
-	home_items = soup.find_all(class_="post-card-item kt-col-6 kt-col-xxl-4")
-	for newapartment in home_items:
-		if  'جنت آباد' in newapartment.get_text():
+
+	for newapartment in load():
+		if  'اندیشه' in newapartment.get_text():
 			if oldapartment!=newapartment.get_text():
 				print(newapartment.get_text())
+				link=newapartment.find('a', class_ = 'kt-post-card kt-post-card--outlined', href = True)
+				if link:
+					checklink(link)
 				oldapartment=newapartment.get_text()
-	# scroll()
-	time.sleep(20)
+	time.sleep(3)
